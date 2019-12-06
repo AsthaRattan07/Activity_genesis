a1636d1b9fca37c4b974410bc73065ace227ad92
AsthaRattan07 <58590130+AsthaRattan07@users.noreply.github.com>
2019-12-06 15:10:49 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,19 @@
+    #include <stdio.h>
+    int addNumbers(int n);
+    int num;
+    int main()
+    {
+   
+        printf("Enter a positive integer: ");
+        scanf("%d", &num);
+        int c=addNumbers(num);
+        printf("Sum = %d",c);
+        return 0;
+    }
+    int addNumbers(int n)
+    {
+        if(n != 0)
+            return n + addNumbers(n-1);
+        else
+            return n;
+    }
